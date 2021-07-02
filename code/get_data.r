ff6e93d0f45b2a98b5c0630e82e774feec99e361
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-07-02 17:28:11 +0100

    new maps

--- code/get_data.r
+++ code/get_data.r
@@ -1,17 +1,21 @@
 # Get dtm data ------------------------------------------------------------
-if(file.exists("data/fal_school_dtm_1.tif")){
-  fal_school = raster("data/fal_school_dtm_1.tif")
+if(file.exists("data/fal_school_dtm.tif")){
+  fal_school = raster("data/fal_school_dtm.tif")
 } else {
   #Get lidar data
   fal_school = get_from_xy(
     xy = c(179011, 033245),
     radius = 500,
-    resolution = 1,
-    model_type = 'DSM',
-    dest_folder = "data",
-    out_name = "fal_school_dtm_1",
-    ras_format = "GTiff"
+    resolution = 0.5,
+    model_type = 'DSM'
   )
 }
 # Get bike route data -----------------------------------------------------
 p = read_sf("data/full_track.gpx", layer = "tracks")
+pp = read_sf("data/full_track.gpx", layer = "track_points") # track points
+resting_area = sf::read_sf("C:/Users/natha/OneDrive/Documents/Resting-Area")
+
+
+
+
+
--- code/main.r
+++ code/main.r
@@ -8,8 +8,8 @@ fal_small = resize_matrix(fal_mat, 0.25)
 
 
 # Create Basemap based on lat long range of site --------------------------
-lat_range = c(50.158314, 50.162294)
-long_range = c(-5.102861,-5.093012)
+lat_range = c(50.159382, 50.161639)
+long_range = c(-5.098562,-5.094943)
 
 utm_bbox = convert_coords(lat = lat_range,
                           long = long_range,
@@ -20,21 +20,28 @@ fal_zoom = crop(fal_school, extent_zoomed)
 fal_zoom_mat = raster_to_matrix(fal_zoom)
 
 basemap = fal_zoom_mat %>%
-  height_shade() %>%
-  add_overlay(sphere_shade(fal_zoom_mat, texture = "desert", colorintensity = 5),
-              alphalayer = 0.5) %>%
-  add_shadow(lamb_shade(fal_zoom_mat), 0) %>%
-  add_shadow(ambient_shade(fal_zoom_mat), 0) %>%
-  add_shadow(texture_shade(
-    fal_zoom_mat,
-    detail = 8 / 10,
-    contrast = 9,
-    brightness = 11
-  ),
-  0.1)
-
-
-
+  sphere_shade(
+    texture = create_texture(
+      "#f5dfca",
+      "#63372c",
+      "#dfa283",
+      "#195f67",
+      "#c2d1cf",
+      cornercolors = c("#ffc500", "#387642", "#d27441", "#296176")
+    ),
+    sunangle = 0,
+    colorintensity = 5
+  ) %>%
+  add_shadow(lamb_shade(fal_zoom_mat), 0.2) %>%
+  add_overlay(
+    generate_altitude_overlay(
+      height_shade(fal_zoom_mat, texture = "#91aaba"),
+      fal_zoom_mat,
+      start_transition = min(fal_zoom_mat) -
+        200,
+      end_transition = max(fal_zoom_mat)
+    )
+  )
 # Create overlays for DTM data --------------------------------------------
 #Route overlay
 fal_lines = st_transform(p, crs = crs(fal_school))
@@ -44,8 +51,28 @@ osm_bbox = c(long_range[1], lat_range[1], long_range[2], lat_range[2])
 fal_highway = opq(osm_bbox) %>%
   add_osm_feature("highway") %>%
   osmdata_sf()
-fal_roads = st_transform(fal_highway$osm_lines, crs = crs(fal_school))
 
+fal_footpaths = subset(fal_roads,
+                       highway == "footway")
+
+fal_cyclepaths = subset(fal_roads,
+                        highway == "cycleway")
+
+fal_roads = subset(
+  fal_roads,
+  highway = c(
+    "unclassified",
+    "secondary",
+    "tertiary",
+    "residential",
+    "service"
+  )
+)
+
+fal_footpaths = st_transform(fal_footpaths$geometry, crs = crs(fal_school))
+fal_roads = st_transform(fal_roads$geometry, crs = crs(fal_school))
+fal_cyclepaths = st_transform(fal_cyclepaths$geometry, crs = crs(fal_school))
+resting_area = st_transform(resting_area$geometry, crs = crs(fal_school))
 
 # Plot map ----------------------------------------------------------------
 basemap %>%
@@ -54,17 +81,22 @@ basemap %>%
       fal_lines,
       extent = extent_zoomed,
       linewidth = 4,
-      color = "white",
+      color = "#2d8a91",
       heightmap = fal_zoom_mat
     )
   ) %>%
   add_overlay(
     generate_line_overlay(
-      fal_roads,
+      resting_area,
       extent = extent_zoomed,
-      linewidth = 2,
-      color = "green",
+      linewidth = 4,
+      color = "black",
       heightmap = fal_zoom_mat
     )
   ) %>%
   plot_map()
+
+#Plot only route
+l = points2line_trajectory(pp)
+plot(l)
+
